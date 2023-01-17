7244ead42287a7791f05a4de22d004d1f08d406c
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2023-01-16 19:24:04 -0500

    1 problems added

--- numOfIslands.py
+++ numOfIslands.py
@@ -7,4 +7,13 @@ def dfs(grid , i ,j):
         dfs(grid[i-1][j])
         dfs(grid[i][j-1]) 
 
+        return 1 def dfs(grid , i ,j):
+        if i < 0 or i > len(grid) or j < 0 or j > len(grid[i]) or grid[i][j] == '0':
+            return 
+
+        dfs(grid[i+1][j])
+        dfs(grid[i][j+1]) 
+        dfs(grid[i-1][j])
+        dfs(grid[i][j-1]) 
+
         return 1 
